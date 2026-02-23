efa8fe029c4373878a67fd9cc8904bc9e5203d35
EvolvePlayStudio <evolveplaystudio@gmail.com>
2026-02-23 20:01:50 -0300

    Coloca anúncio da AdsTerra na tela de quiz

--- static/utils.js
+++ static/utils.js
@@ -2,7 +2,8 @@ export const pontuacaoTemaPadraoVisitantes = 1800;
 export const dificuldadesOrdenadas = ['Fácil', 'Médio', 'Difícil', 'Extremo'];
 export const temas_disponiveis = ["Artes", "Astronomia", "Biologia", "Esportes", "Filosofia", "Física", "Geografia", "História", "Mídia", "Música", "Química", "Variedades"];
 export const idsReservados = [4, 6, 16];
-export const idVisitanteAdmin = '605720b7-c72f-4b18-9b73-c3615bfce897'
+export const idVisitanteAdmin = '605720b7-c72f-4b18-9b73-c3615bfce897';
+export const idsVisitantesReservados = ['605720b7-c72f-4b18-9b73-c3615bfce897'];
 
 let idUsuario;
 let idVisitante;
